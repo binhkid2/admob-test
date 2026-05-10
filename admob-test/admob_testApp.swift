6a1303f1ff4441971464ab9656c3407ee3a9a9ec
binhkid2 <binhkid2@gmail.com>
2026-05-10 21:41:56 +0900

    Initial Commit

---
+++ admob-test/admob_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  admob_testApp.swift
+//  admob-test
+//
+//  Created by binhkid2 on 2026/05/10.
+//
+
+import SwiftUI
+
+@main
+struct admob_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
